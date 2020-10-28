16c267b3ed88f0fe66c7747a0a91f9c1a8b057dd
vinibispo <vini.bispo015@gmail.com>
2020-10-27 23:20:42 -0300

    Adding theme

---
+++ src/pages/_theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#0081cb',
+    },
+    secondary: {
+      main: '#02020a',
+    },
+    background: {
+      default: '#333646',
+    },
+  },
+});
