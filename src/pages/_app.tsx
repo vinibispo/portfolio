751b5442d8d67694a7bd20711989cdd485e77e63
vinibispo <vini.bispo015@gmail.com>
2020-10-27 23:15:47 -0300

    Adding next default

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { AppProps } from 'next/app';
+import React, { useEffect } from 'react';
+import { theme } from './_theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <CssBaseline />
+    </ThemeProvider>
+  );
+};
+export default MyApp;
